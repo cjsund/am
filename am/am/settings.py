27344e4294103cf4a95f60e6c183e1b734e9a3f9
cjsund <cjsund@126.com>
2013-05-31 12:38:33 +0800

    update am/settings.py ...

--- am/am/settings.py
+++ am/am/settings.py
@@ -124,7 +124,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'models',
+    #'models',
+    'books',
+    'assets',
 )
 
 # A sample logging configuration. The only tangible logging
